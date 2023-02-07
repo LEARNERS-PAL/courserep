b7e390c312d0a6c19d4d3389ca765e3c11c1adc3
qbentil <bentilshadrack72@gmail.com>
2023-02-07 17:31:13 +0000

    Update App.ts, Listen to client on ready

--- App.ts
+++ App.ts
@@ -1,14 +1,29 @@
+import { Client } from 'whatsapp-web.js';
 import qrcode from 'qrcode-terminal';
 
-import { Client } from 'whatsapp-web.js';
 const client = new Client({ puppeteer: { headless: true } });
 
+// GLOBAL VARIABLES
+let qr_code = null as string | null;
+
+
 client.on('qr', (qr: string) => {
+    // Generate and scan this code with your phone
+    qr_code = qr;
+    console.log("QR RECEIVED", qr);
     qrcode.generate(qr, { small: true });
 });
 
 client.on('ready', () => {
     console.log('Client is ready!');
+});
+
+
+client.on('message', message => {
+    console.log(message);
+    if (message.body === '!ping') {
+        message.reply('pong🚀');
+    }
 });
 
 client.initialize();
