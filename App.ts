bb3f3343edd8f2b7a0199e9fd616f5f26f9c7dbc
qbentil <bentilshadrack72@gmail.com>
2023-02-08 16:28:31 +0000

    Setup MongoStore to keep sessions

--- App.ts
+++ App.ts
@@ -1,29 +1,87 @@
-import { Client } from 'whatsapp-web.js';
+import WAWebJS, { Client, RemoteAuth } from 'whatsapp-web.js';
+
+import DBCONNECT from './mongodb/_init';
+import { MongoStore } from 'wwebjs-mongo';
+import mongoose from 'mongoose';
 import qrcode from 'qrcode-terminal';
 
-const client = new Client({ puppeteer: { headless: true } });
-
-// GLOBAL VARIABLES
 let qr_code = null as string | null;
+let isDevMode = false;
 
 
-client.on('qr', (qr: string) => {
-    // Generate and scan this code with your phone
-    qr_code = qr;
-    console.log("QR RECEIVED", qr);
-    qrcode.generate(qr, { small: true });
-});
+(async () => {
+    await DBCONNECT();
+    const store = new MongoStore({ mongoose: mongoose });
+    const client = new Client({
+        puppeteer: {
+            headless: isDevMode ? false : true,
+            args: isDevMode ? [] : [
+                "--no-sandbox",
+                '--disable-gpu',
+                '--disable-dev-shm-usage',
+                '--disable-setuid-sandbox',
+            ]
+        },
+        authStrategy: new RemoteAuth({
+            store: store,
+            backupSyncIntervalMs: 300000
+        })
+    });
 
-client.on('ready', () => {
-    console.log('Client is ready!');
-});
+    const connection = mongoose.connection;
+
+    // client  on qr
+    client.on('qr', (qr) => {
+        qr_code = qr;
+        qrcode.generate(qr, { small: true });
+    });
+
+    // client on ready
+    client.on('ready', async () => {
+        console.log('Client is ready!');
+        console.log(await client.getWWebVersion());
+        qr_code = "Done";
+
+        const allChats = await client.getChats();
+        const chat = allChats.find((chat) => {
+            return chat.name.toLocaleLowerCase().includes(isDevMode ? "testing" : 'github');
+        });
 
 
-client.on('message', message => {
-    console.log(message);
-    if (message.body === '!ping') {
-        message.reply('pong🚀');
-    }
-});
+        const startMessage = await chat?.sendMessage("I am back online ✅")
+        const botContact = await startMessage?.getContact()
+        console.log(botContact);
+    })
 
-client.initialize();
+    // client on authenticated
+    client.on("authenticated", () => {
+        console.log("AUTHENTICATED");
+    });
+
+    client.on("auth_failure", (msg) => {
+        // Fired if session restore was unsuccessful
+        console.error("AUTHENTICATION FAILURE", msg);
+    });
+
+
+    connection.once("open", async () => {
+        console.log(">> Mongo database connection established successfully <<");
+        // await initModels();
+    });
+    connection.on("error", () => {
+        console.error.bind(console, ">> connection error <<");
+    });
+
+    // ping client
+    client.on('message', async (message) => {
+        if(message.body === '!ping') {
+            client.sendMessage(message.from, 'pong pong pong🚀');
+        }
+    })
+
+    // INIT CLIENT
+    client.initialize();
+
+
+})();
+
